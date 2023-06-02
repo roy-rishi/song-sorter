664cade2c9ebf1874a43ab93b1427e1bc5907845
Rishi <72419270+roy-rishi@users.noreply.github.com>
2023-06-01 20:44:02 -0700

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,3 +1,3 @@
-client_id="be311faa71b543df8c75ba972dfd2320"
-client_secret="cba85a4ac4dc4e36aa6514743101b8a8"
-redirect_url="http://localhost:5173/callback"
+client_id="your_client_id"
+client_secret="your_client_secret"
+redirect_url="your_redirect_url"
